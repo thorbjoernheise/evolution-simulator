35e08ce86871b714d6e094068ed4a51364f43438
Thorbjörn Heise <thorbjoern.heise@gmail.com>
2023-01-05 22:07:30 +0100

    Rebuilt some functions, implemented 3d scatter pl

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from deap import base
 from deap import creator
 from deap import tools
 from sklearn.cluster import KMeans
+import csv
 
 # fitness function with set of parameters
 def evaluate_fitness(parameters):
@@ -13,9 +14,9 @@ def evaluate_fitness(parameters):
 
 # Function that returns True/False if two individuals can reproduce
 def can_reproduce(ind1, ind2):
-    size_threshold = 5
-    strength_threshold = 5
-    speed_threshold = 5
+    size_threshold = 20
+    strength_threshold = 20
+    speed_threshold = 20
     size_diff = abs(ind1[0] - ind2[0])
     strength_diff = abs(ind1[1] - ind2[1])
     speed_diff = abs(ind1[2] - ind2[2])
@@ -41,9 +42,9 @@ creator.create("Individual", Individual)
 
 # Defining population
 toolbox = base.Toolbox()
-toolbox.register("attr_size", random.randint, 0, 10)
-toolbox.register("attr_strength", random.randint, 0, 10)
-toolbox.register("attr_speed", random.randint, 0, 10)
+toolbox.register("attr_size", random.randint, 0, 100)
+toolbox.register("attr_strength", random.randint, 0, 100)
+toolbox.register("attr_speed", random.randint, 0, 100)
 toolbox.register("individual", tools.initCycle, Individual,
                  (toolbox.attr_size, toolbox.attr_strength, toolbox.attr_speed), n=1)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
@@ -56,7 +57,7 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 
 # Evolve the population
 population = toolbox.population(n=200)
-NGEN = 200
+NGEN = 500
 for generation in range(NGEN):
     # Check if the population has diverged enough to form new species
     species = []
@@ -81,27 +82,53 @@ for generation in range(NGEN):
 # Print the final population
 print(population)
 
-# Define a function that takes in an individual and returns the species it belongs to
-def distance(ind1, ind2):
-    size_diff = abs(ind1[0] - ind2[0])
-    strength_diff = abs(ind1[1] - ind2[1])
-    speed_diff = abs(ind1[2] - ind2[2])
-    return size_diff + strength_diff + speed_diff
+def get_species(ind, population):
+    # Initialize a list of species
+    species = []
+    # Iterate over the population and assign individuals to species based on their ability to reproduce
+    for individual in population:
+        found_species = False
+        for s in species:
+            if can_reproduce(individual, s[0]):
+                s.append(individual)
+                found_species = True
+                break
+        if not found_species:
+            species.append([individual])
+    # Return the name of the species that the input individual belongs to
+    for i, s in enumerate(species):
+        if ind in s:
+            return 'Species {}'.format(i+1)
+    return 'Unclassified'
 
-# Define a function that takes in an individual and returns the species it belongs to
-def get_species(ind, population, k):
-    # Use k-means clustering to divide the population into k clusters
-    X = [[ind[0], ind[1], ind[2]] for ind in population]
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
-    # Assign the individual to the cluster that it belongs to
-    cluster_idx = kmeans.predict([[ind[0], ind[1], ind[2]]])[0]
-    return chr(ord('A') + cluster_idx)
+# Get the species names for each individual in the population
+species_names = [get_species(ind, population) for ind in population]
 
-# Scatter plot of the individuals
-k = 5
-species = [get_species(ind, population, k) for ind in population]
-colors = { chr(ord('A') + i): plt.cm.Set1(i / k) for i in range(k) }
-plt.scatter([ind[0] for ind in population], [ind[1] for ind in population], c=[colors[s] for s in species])
-plt.xlabel("Size")
-plt.ylabel("Strength")
+# Initialize the colors dictionary
+colors = { s: plt.cm.Set1(i / len(set(species_names))) for i, s in enumerate(set(species_names)) }
+
+# Create the 3D scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter([ind[0] for ind in population], [ind[1] for ind in population], [ind[2] for ind in population], c=[colors[s] for s in species_names])
+ax.set_xlabel("Size")
+ax.set_ylabel("Strength")
+ax.set_zlabel("Speed")
 plt.show()
+
+#Writing the data to population.csv
+# Open a file in write mode
+with open('population.csv', 'w', newline='') as csvfile:
+  # Create a CSV writer object
+  writer = csv.writer(csvfile)
+  
+  # Write the header row
+  writer.writerow(['size', 'strength', 'speed', 'species'])
+  
+  # Iterate over the population
+  for ind in population:
+    # Get the species name for the individual
+    species = get_species(ind, population)
+    # Write the individual's data to the CSV file
+    writer.writerow(list(ind) + [species])
+
